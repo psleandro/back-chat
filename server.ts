8c4091d0dc64c3de5c76696902d7d60c9a2eca9b
Leandro Santos <lsaantos@outlook.com>
2022-05-01 19:09:02 -0300

    adjusts events to peerJs

--- server.ts
+++ server.ts
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { createServer } = require('http');
 const { Server } = require('socket.io');
-const cors  = require('cors');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
@@ -23,24 +23,25 @@ let messages = [];
 
 io.on('connection', socket => {
 
-    socket.on('join room', (roomId, user) => {
-        Object.assign(user, {socketId: socket.id});
+    socket.on('join-room', (roomId, user) => {
+        Object.assign(user, { socketId: socket.id });
         socket.join(roomId);
 
-        if(rooms[roomId]){
+        if (rooms[roomId]) {
             rooms[roomId].push(user);
         } else {
             rooms[roomId] = [user];
             messages[roomId] = [];
         }
-        
-        const usersRoom = rooms[roomId].filter(u => u.socketId !== socket.id);
-        socket.emit('all users connected', usersRoom);
+
+        const usersRoom = rooms[roomId].filter(u => u.socketId !== socket.id) || [];
+        socket.emit('get-all-users-connected', usersRoom);
         socket.emit('all messages history', messages[roomId]);
 
-        // socket.to(roomId).emit('user-joined', {...user, socketId: socket.id});
+        socket.to(roomId).emit('user-joined', { ...user, socketId: socket.id });
 
         console.log(`now users in the room ${roomId} is: `, rooms[roomId]);
+        // console.log(`now users in the room ${roomId} is: `, usersRoom);
 
         socket.on('sendMessage', data => {
             console.log(`received message in the room ${roomId}:`, data);
@@ -60,24 +61,35 @@ io.on('connection', socket => {
         socket.on('response user-joined request', data => {
             console.log(`my socket id is: `, socket.id);
             console.log(`returning response from: ${socket.id} to ${data.callerId} `, data);
-            io.to(data.callerId).emit('receiving final signal response', {signal: data.signal, userToSignalId: socket.id})
+            io.to(data.callerId).emit('receiving final signal response', { signal: data.signal, userToSignalId: socket.id })
         })
 
         socket.on('disconnect', data => {
             console.log('user disconnected!', data);
-            rooms[roomId] = rooms[roomId].filter(user =>user.socketId !== socket.id )
-            socket.to(roomId).emit('user-disconnected', socket.id);
+            rooms[roomId] = rooms[roomId].filter(user => user.socketId !== socket.id)
+            socket.to(roomId).emit('user-disconnected', user.peerId);
             console.log(`now users in the room ${roomId} is: ${rooms[roomId]}`);
         })
 
 
         socket.on('emitting peer signal', data => {
-           console.log('new user added on peer with: ', data);
-           io.to(data.userToSignal).emit('user joined in call', {
-               callerId: data.callerId
-           })
+            console.log('new user added on peer with: ', data);
+            io.to(data.userToSignal).emit('user joined in call', {
+                callerId: data.callerId
+            })
         })
     });
+
+    socket.on('req-room-invite-verification', (roomId) => {
+        const room = rooms[roomId];
+
+        if (room) {
+            socket.emit('res-room-invite-verification', true);
+        } else {
+            socket.emit('res-room-invite-verification', false);
+        }
+    });
+
 });
 
 const port = process.env.PORT || 5000;
